c708b0a66ade0585ea09a957bdf74392593f8304
ElisaBreeze <elisamabre@gmail.com>
2023-12-18 18:19:25 +0000

    Price receiver added to event-store-builder

--- event-store-builder/src/main/java/org/ulpgc/dacd/Main.java
+++ event-store-builder/src/main/java/org/ulpgc/dacd/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) throws StoreException {
         WeatherEventReceiver weatherEventReceiver = new WeatherEventReceiver(args[0]);
         weatherEventReceiver.messageReceiver();
+
+        PriceEventReceiver priceEventReceiver = new PriceEventReceiver(args[1]);
+        priceEventReceiver.messageReceiver();
     }
 }
---
+++ event-store-builder/src/main/java/org/ulpgc/dacd/PriceEventReceiver.java
@@ -0,0 +1,89 @@
+package org.ulpgc.dacd;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class PriceEventReceiver {
+    private final String serverURL = ActiveMQConnection.DEFAULT_BROKER_URL;
+    private final String eventStoringPath;
+
+    public PriceEventReceiver(String eventStoringPath){ this.eventStoringPath = eventStoringPath; }
+
+    public void messageReceiver() throws StoreException {
+        try {
+            Connection connection = new ActiveMQConnectionFactory(serverURL).createConnection();
+            connection.setClientID("event-store-builder-price"); //TODO puede ser el mismo client?
+            connection.start();
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Topic topicName = session.createTopic("prediction.Price");
+            MessageConsumer consumer = session.createDurableSubscriber(topicName,"eventStoreBuilder-prediction.Price");
+            //TODO from here on doesnt work
+            consumer.setMessageListener(message -> {
+                try {
+                    messageCreator(message);
+                } catch (StoreException exception) {
+                    throw new RuntimeException(exception);
+                }
+            });
+        } catch (JMSException exception) {
+            throw new StoreException(exception.getMessage(), exception);
+        }
+    }
+
+    private void messageCreator(Message message) throws StoreException {
+        if (message instanceof TextMessage textMessage) {
+            try {
+                saveEvent(textMessage.getText());
+            } catch (JMSException exception) {
+                throw new StoreException(exception.getMessage(), exception);
+            }
+        }
+    }
+
+    private void saveEvent(String event) throws StoreException {
+        File file = fileOf(event);
+        try (FileWriter fileWriter = new FileWriter(file, true)) {
+            fileWriter.write(event + "\n");
+        } catch (IOException exception) {
+            throw new StoreException(exception.getMessage(), exception);
+        }
+    }
+
+    private File fileOf(String event) throws StoreException{
+        JsonObject jsonObject = new Gson().fromJson(event, JsonObject.class);
+        String tsNotFormatted = jsonObject.get("ts").getAsString();
+        String ss = jsonObject.get("ss").getAsString();
+        String ts = dateFormatter(tsNotFormatted);
+        Path filePath = Paths.get(eventStoringPath, ss, ts + ".events");
+        directoryCreator(filePath.getParent());
+        return filePath.toFile();
+    }
+
+    private String dateFormatter(String tsNotFormatted) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ISO_DATE_TIME;
+        LocalDate localDateTs = LocalDate.parse(tsNotFormatted, dateTimeFormatter);
+        return localDateTs.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+    }
+
+    private void directoryCreator(Path directoryPath) throws StoreException {
+        if (!Files.exists(directoryPath)) {
+            try {
+                Files.createDirectories(directoryPath);
+            } catch (IOException exception) {
+                throw new StoreException(exception.getMessage(), exception);
+            }
+        }
+    }
+}
